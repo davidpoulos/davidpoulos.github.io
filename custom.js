fd3d19dd0c9b4e2a8753b666f3d044ec7386de09
David Poulos <davidpoulos@ymail.com>
2017-06-24 22:31:33 -0500

    UI interactive changes

---
+++ custom.js
@@ -0,0 +1,35 @@
+
+// window.onload = function() {
+//   const element = document.getElementById('f_e_d');
+//
+//   const changeColor = function(){
+//     element.style.color = "blue";
+//   };
+//
+//   const changeColor2 = function(){
+//     element.style.color = "black";
+//   };
+//
+//   element.addEventListener("mouseover", changeColor);
+//   element.addEventListener("mouseout", changeColor2);
+//
+// };
+
+// window.onload = function() {
+// let flag = false;
+// const element = document.getElementById("portfolio_name");
+//
+// setInterval(function() {
+//
+//   if(!flag) {
+//     element.style.color="yellow";
+//     flag = true;
+//   }
+//   else {
+//     element.style.color="white";
+//     flag = false;
+//   }
+//
+//  }, 3000);
+//
+// };
