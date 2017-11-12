49b75db7b3accdbc96122854515c970cce15391a
David Poulos <davidpoulos@ymail.com>
2017-11-12 15:03:45 -0600

    Adding some flare

--- custom.js
+++ custom.js
@@ -1,35 +1,32 @@
+$(document).ready(function() {
 
-// window.onload = function() {
-//   const element = document.getElementById('f_e_d');
-//
-//   const changeColor = function(){
-//     element.style.color = "blue";
-//   };
-//
-//   const changeColor2 = function(){
-//     element.style.color = "black";
-//   };
-//
-//   element.addEventListener("mouseover", changeColor);
-//   element.addEventListener("mouseout", changeColor2);
-//
-// };
 
-// window.onload = function() {
-// let flag = false;
-// const element = document.getElementById("portfolio_name");
-//
-// setInterval(function() {
-//
-//   if(!flag) {
-//     element.style.color="yellow";
-//     flag = true;
-//   }
-//   else {
-//     element.style.color="white";
-//     flag = false;
-//   }
-//
-//  }, 3000);
-//
-// };
+const $about = $('#about_me');
+const $front_end = $('#f_e_d');
+const $back_end = $('#b_e_d');
+const $personal_projects = $('#p_p');
+
+let h2_list = [$about, $front_end, $back_end, $personal_projects];
+
+let previous = 0;
+let current = 1;
+
+setInterval(() => {
+    h2_list[previous].css("color", "#ff48c4");
+    h2_list[current].css("color", "white");
+
+    previous = previous + 1;
+    current = current + 1;
+
+    if(current >= h2_list.length) {
+        current = 0;
+    }
+
+    if(previous >= h2_list.length) {
+        previous = 0;
+    }
+
+}, 1000);
+
+
+});
